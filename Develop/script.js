1b5cdcf872e0014fd8269e3a9765a2346fdb6977
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-01-24 19:12:18 -0700

    first coding of script.js

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,20 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var createPassword = [];
+var userPasswordLength = "";
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var numeric = "1234567890";
+var special = "!?@#$%^&*"
+
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  createPassword = [];
+  generatePassword();
 }
+
+function generatePassword() {}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
