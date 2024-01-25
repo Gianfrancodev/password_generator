1ead38ab83f409745b2f35f4045ae02c845ddb14
Gianfranco De Vettori <Ako@Akos-Mac-2.local>
2024-01-24 21:28:45 -0700

    building out window questions

--- Develop/script.js
+++ Develop/script.js
@@ -14,7 +14,42 @@ function writePassword() {
   generatePassword();
 }
 
-function generatePassword() {}
+function generatePassword() {
+  passwordLength();
+  passwordLowercase();
+  passwordUppercase();
+  passwordNumeric();
+  passwordSpecial();
+  createUserPassword();
+}
+
+function passwordLength() {
+  var userInputLength = window.prompt("Choose your password length. \n It must be a minumum of 8 and maximum of 128 characters. \n");
+  if (userInputLength === null) {
+    throw "User chose to not continue";
+  }
+  else if (userInputLength > 128) {
+    alert("Please try again, your password was too long. \n Must not be greater than 128.");
+    passwordLength();
+  } else if (userInputLength < 8) {
+    alert("Please try again, your password was too short. \n Must not be less than 8.");
+    passwordLength();
+  } else {
+    userPasswordLength = userInputLength;
+  }
+}
+
+function passwordLowercase() {
+  if (window.confirm("Would you like LOWERCASE letters included in your password? \n 'YES' to include lowercase. \n 'NO' for no lowercase.")) {
+    createPassword.push(lowercase);
+  }
+}
+
+function passwordUppercase() {
+  if (window.confirm("Would you like UPPERCASE letters included in your password? \n 'YES' to include uppercase. \n 'NO' for no uppercase.")) {
+    createPassword.push(uppercase);
+  }
+}
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
